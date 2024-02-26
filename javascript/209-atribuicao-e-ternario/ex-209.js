7d9a8af68b34b47e5e62024040539a71d2c5a8e8
@fmota-dev <fmota.web@gmail.com>
2024-02-25 23:37:22 -0300

    aula 209 - atribuição-e-ternario

---
+++ javascript/209-atribuicao-e-ternario/ex-209.js
@@ -0,0 +1,15 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var valor = 1000;
+valor += 500;
+console.log(valor)
+
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito = (possuiCarro && possuiCasa) ? true : false;
+
+console.log(darCredito)
